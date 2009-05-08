72952e14f27c6814d7f407349c3ce6482b13e44a
Simon Rozet <simon@rozet.name>
2009-05-08 11:39:06 +0200

    Get rid of stub-ish test

--- test/bobette_test.rb
+++ test/bobette_test.rb
@@ -7,19 +7,6 @@ class BobetteTest < BobetteTestCase
       "repository" => {"url" => "git://github.com/sr/bobette.git"} }
   end
 
-  setup do
-    Bobette.buildable = FakeBuildable
-
-    @buildable = FakeBuildable.new(payload)
-  end
-
-  it "makes a buildable from the payload and builds it with Bob" do
-    stub(FakeBuildable).new(payload) { @buildable }
-    stub(Bob).build(@buildable, ["8667fbb"]) { nil }
-
-    assert post("/", :payload => payload.to_json).ok?
-  end
-
   test "400 with invalid JSON" do
     assert post("/").client_error?
     assert post("/", :payload => "</3").client_error?
--- test/helper.rb
+++ test/helper.rb
@@ -1,6 +1,5 @@
 require "test/unit"
 require "contest"
-require "rr"
 require "rack/test"
 require "ruby-debug"
 require "integrity/notifier/test"
@@ -9,30 +8,11 @@ require File.dirname(__FILE__) + "/../lib/bobette"
 require File.dirname(__FILE__) + "/../lib/integrity/buildable_project"
 require File.dirname(__FILE__) + "/helper/git_helper"
 
-class FakeBuildable
-  def initialize(payload)
-    @payload = payload
-  end
-
-  def uri
-    @payload["repository"]["url"]
-  end
-
-  def kind
-    URI(uri).scheme
-  end
-
-  def branch
-    @payload["ref"].split("/").last
-  end
-end
-
 class BobetteTestCase < Test::Unit::TestCase
   class << self
     alias_method :it, :test
   end
 
-  include RR::Adapters::TestUnit
   include Rack::Test::Methods
   include TestHelper
 
